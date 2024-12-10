3491d662f42e7bc08c7ddb8ff8a431f63c42e898
Sergey Gavrilenko <gryroach@gmail.com>
2024-12-10 22:07:08 +0300

    Task 10: добавление ручек получения и изменения роли пользователя

--- src/exceptions/user_exceptions.py
+++ src/exceptions/user_exceptions.py
@@ -2,3 +2,9 @@ class UserAlreadyExistsError(Exception):
     def __init__(self, message: str = "User already exists"):
         self.message = message
         super().__init__(self.message)
+
+
+class UserDoesNotExistsError(Exception):
+    def __init__(self, message: str = "User does not exists"):
+        self.message = message
+        super().__init__(self.message)
--- src/repositories/user.py
+++ src/repositories/user.py
@@ -1,9 +1,6 @@
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from models import User
 from repositories.base import RepositoryDB
-from schemas.entity import UserCreate, UserInDB
+from schemas.user import UserCreate, UserInDB
 
 
 class UserRepository(RepositoryDB[User, UserCreate, UserInDB]):
