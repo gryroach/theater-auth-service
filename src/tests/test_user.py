b68f029b6919872837110217369fa1eed94be1d9
Sergey Gavrilenko <gryroach@gmail.com>
2024-12-13 23:29:01 +0300

    Task 10: добавление тестов проверки прав

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -11,10 +11,10 @@ from services.roles import Roles
 
 
 @pytest_asyncio.fixture
-async def create_user(session: AsyncSession) -> User:
-    """Создание пользователя"""
+async def create_admin_user(session: AsyncSession) -> User:
+    """Создание админа"""
     user = User(
-        login="testuser",
+        login="admin",
         role=Roles.admin.name,
         password="123",
         first_name="Tester",
@@ -27,10 +27,40 @@ async def create_user(session: AsyncSession) -> User:
 
 
 @pytest_asyncio.fixture
-async def login_user(client: AsyncClient, create_user: User) -> dict:
+async def create_regular_user(session: AsyncSession) -> User:
+    """Создание пользователя"""
+    user = User(
+        login="testuser",
+        role=Roles.regular_user.name,
+        password="123",
+        first_name="Tester",
+        last_name="Pass",
+    )
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
+
+
+@pytest_asyncio.fixture
+async def login_admin_user(
+        client: AsyncClient, create_admin_user: User
+) -> dict:
+    """Выполняет логин админа и возвращает токены доступа."""
+    login_data = {
+        "login": create_admin_user.login,
+        "password": "123",
+    }
+    response = await client.post("api/v1/auth/login", json=login_data)
+    assert response.status_code == status.HTTP_200_OK
+    return response.json()
+
+
+@pytest_asyncio.fixture
+async def login_user(client: AsyncClient, create_regular_user: User) -> dict:
     """Выполняет логин пользователя и возвращает токены доступа."""
     login_data = {
-        "login": create_user.login,
+        "login": create_regular_user.login,
         "password": "123",
     }
     response = await client.post("api/v1/auth/login", json=login_data)
@@ -39,15 +69,28 @@ async def login_user(client: AsyncClient, create_user: User) -> dict:
 
 
 @pytest.mark.asyncio
+async def test_login_user_wrong_pass(
+        client: AsyncClient, create_regular_user: User
+) -> None:
+    """Тест неправильного пароля."""
+    login_data = {
+        "login": create_regular_user.login,
+        "password": "wrong",
+    }
+    response = await client.post("api/v1/auth/login", json=login_data)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.asyncio
 async def test_get_user_role(
-    client: AsyncClient, create_user: User, login_user: dict
+    client: AsyncClient, create_admin_user: User, login_admin_user: dict
 ) -> None:
     """
     Тест для получения роли пользователя
     """
-    headers = {"Authorization": f"Bearer {login_user['access_token']}"}
+    headers = {"Authorization": f"Bearer {login_admin_user['access_token']}"}
     response = await client.get(
-        f"/api/v1/users/{create_user.id}/role",
+        f"/api/v1/users/{create_admin_user.id}/role",
         headers=headers,
     )
     assert response.status_code == status.HTTP_200_OK
@@ -57,27 +100,42 @@ async def test_get_user_role(
 
 
 @pytest.mark.asyncio
+async def test_get_user_role_by_regular_user(
+    client: AsyncClient, create_regular_user: User, login_user: dict
+) -> None:
+    """
+    Тест для получения роли пользователя обычным пользователем
+    """
+    headers = {"Authorization": f"Bearer {login_user['access_token']}"}
+    response = await client.get(
+        f"/api/v1/users/{create_regular_user.id}/role",
+        headers=headers,
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio
 async def test_get_user_role_unauthorized(
-        client: AsyncClient, create_user: User
+        client: AsyncClient, create_admin_user: User
 ) -> None:
     """
     Тест авторизации при получении роли пользователя
     """
-    response = await client.get(f"/api/v1/users/{create_user.id}/role")
+    response = await client.get(f"/api/v1/users/{create_admin_user.id}/role")
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
 @pytest.mark.asyncio
 async def test_set_user_role(
-    client: AsyncClient, create_user: User, login_user: dict
+    client: AsyncClient, create_admin_user: User, login_admin_user: dict
 ) -> None:
     """
     Тест для обновления роли пользователя
     """
     new_role = {"role": "moderator"}
-    headers = {"Authorization": f"Bearer {login_user['access_token']}"}
+    headers = {"Authorization": f"Bearer {login_admin_user['access_token']}"}
     response = await client.patch(
-        f"api/v1/users/{create_user.id}/role",
+        f"api/v1/users/{create_admin_user.id}/role",
         json=new_role,
         headers=headers,
     )
@@ -88,15 +146,32 @@ async def test_set_user_role(
 
 
 @pytest.mark.asyncio
+async def test_set_user_role_by_regular_user(
+    client: AsyncClient, create_regular_user: User, login_user: dict
+) -> None:
+    """
+    Тест для обновления роли пользователя обычным пользователем
+    """
+    new_role = {"role": "moderator"}
+    headers = {"Authorization": f"Bearer {login_user['access_token']}"}
+    response = await client.patch(
+        f"api/v1/users/{create_regular_user.id}/role",
+        json=new_role,
+        headers=headers,
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio
 async def test_set_user_role_unauthorized(
-    client: AsyncClient, create_user: User
+    client: AsyncClient, create_admin_user: User
 ) -> None:
     """
     Тест авторизации при обновлении роли пользователя
     """
     new_role = {"role": "moderator"}
     response = await client.patch(
-        f"api/v1/users/{create_user.id}/role",
+        f"api/v1/users/{create_admin_user.id}/role",
         json=new_role,
     )
     assert response.status_code == status.HTTP_403_FORBIDDEN
@@ -104,12 +179,12 @@ async def test_set_user_role_unauthorized(
 
 @pytest.mark.asyncio
 async def test_get_user_role_not_found(
-        client: AsyncClient, login_user: dict
+        client: AsyncClient, login_admin_user: dict
 ) -> None:
     """
     Тест для проверки ошибки при получении роли несуществующего пользователя
     """
-    headers = {"Authorization": f"Bearer {login_user['access_token']}"}
+    headers = {"Authorization": f"Bearer {login_admin_user['access_token']}"}
     non_existent_user_id = UUID("123e4567-e89b-12d3-a456-426614174000")
     response = await client.get(
         f"api/v1/users/{non_existent_user_id}/role",
@@ -134,12 +209,12 @@ async def test_get_user_role_not_found_unauthorized(
 
 @pytest.mark.asyncio
 async def test_set_user_role_not_found(
-        client: AsyncClient, login_user: dict
+        client: AsyncClient, login_admin_user: dict
 ) -> None:
     """
     Тест для проверки ошибки при обновлении роли несуществующего пользователя
     """
-    headers = {"Authorization": f"Bearer {login_user['access_token']}"}
+    headers = {"Authorization": f"Bearer {login_admin_user['access_token']}"}
     non_existent_user_id = UUID("123e4567-e89b-12d3-a456-426614174000")
     new_role = {"role": "moderator"}
     response = await client.patch(
@@ -170,15 +245,15 @@ async def test_set_user_role_not_found_unauthorized(
 
 @pytest.mark.asyncio
 async def test_set_user_role_invalid(
-    client: AsyncClient, create_user: User, login_user: dict
+    client: AsyncClient, create_admin_user: User, login_admin_user: dict
 ) -> None:
     """
     Тест для проверки ошибки при обновлении роли на несуществующую роль
     """
-    headers = {"Authorization": f"Bearer {login_user['access_token']}"}
+    headers = {"Authorization": f"Bearer {login_admin_user['access_token']}"}
     invalid_role = {"role": "invalid_role"}
     response = await client.patch(
-        f"api/v1/users/{create_user.id}/role",
+        f"api/v1/users/{create_admin_user.id}/role",
         json=invalid_role,
         headers=headers,
     )
@@ -190,14 +265,14 @@ async def test_set_user_role_invalid(
 
 @pytest.mark.asyncio
 async def test_set_user_role_invalid_unauthorized(
-    client: AsyncClient, create_user: User
+    client: AsyncClient, create_admin_user: User
 ) -> None:
     """
     Тест авторизации при обновлении роли на несуществующую роль
     """
     invalid_role = {"role": "invalid_role"}
     response = await client.patch(
-        f"api/v1/users/{create_user.id}/role",
+        f"api/v1/users/{create_admin_user.id}/role",
         json=invalid_role,
     )
     assert response.status_code == status.HTTP_403_FORBIDDEN
